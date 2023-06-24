65bbd8c336eed12fc1c4df5739154b392238ec5a
Claudia Leiva <cleiva.hz@gmail.com>
2023-06-23 20:01:04 -0400

    Subiendo Desafio 2 de Modulo Backend con Node y Express

---
+++ index.js
@@ -0,0 +1,46 @@
+// Levantando el servidor
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.listen(3000, console.log("¡Servidor encendido!"));
+app.use(express.json())
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+// Leyendo canciones.json
+app.get("/canciones", (req, res) => {
+    const songs = JSON.parse(fs.readFileSync("canciones.json"));
+    res.json(songs);
+});
+
+// Creando canciones.json
+app.post("/canciones", (req, res) => {
+    const song = req.body;
+    const songs = JSON.parse(fs.readFileSync("canciones.json"));
+    songs.push(song);
+    fs.writeFileSync("canciones.json", JSON.stringify(songs));
+    res.send("Canción agregada con éxito!");
+});
+
+// Borrando canciones en canciones.json
+app.delete("/canciones/:id", (req, res) => {
+    const { id } = req.params;
+    const songs = JSON.parse(fs.readFileSync("canciones.json"));
+    const index = songs.findIndex((s) => s.id == id);
+    songs.splice(index, 1);
+    fs.writeFileSync("canciones.json", JSON.stringify(songs));
+    res.send("Canción eliminada con éxito");
+});
+
+// Actualizando una Canción en canciones.json
+app.put("/canciones/:id", (req, res) => {
+    const { id } = req.params;
+    const song = req.body;
+    const songs = JSON.parse(fs.readFileSync("canciones.json"));
+    const index = songs.findIndex((s) => s.id == id);
+    songs[index] = song;
+    fs.writeFileSync("canciones.json", JSON.stringify(songs));
+    res.send("Canción modificada con éxito");
+});
